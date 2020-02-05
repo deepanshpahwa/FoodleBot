9f2fd2f9a9663f54cb67ff6f0dccd18af482e87a
deepansh <deepanshpahwa96@gmail.com>
2020-02-05 13:30:33 +0530

    changes

--- src/main/java/org/telegram/BotConfig.java
+++ src/main/java/org/telegram/BotConfig.java
@@ -7,30 +7,9 @@ package org.telegram;
  * @date 20 of June of 2015
  */
 public class BotConfig {
-    public static final String WEATHER_TOKEN = "<token>";
-    public static final String WEATHER_USER = "weatherbot";
+    public static final String WEATHER_TOKEN = "bot1024199400:AAHpTIN24yV0OAVSP35HvRin05vMzLHudyo";
+    public static final String WEATHER_USER = "foodleNTUbot";
 
-    public static final String TRANSIFEX_TOKEN = "<token>";
-    public static final String TRANSIFEX_USER = "TGlanguagesbot";
-
-    public static final String FILES_TOKEN = "<token>";
-    public static final String FILES_USER = "filesbot";
-
-    public static final String DIRECTIONS_TOKEN = "<token>";
-    public static final String DIRECTIONS_USER = "directionsbot";
-
-    public static final String CHANNEL_TOKEN = "<token>";
-    public static final String CHANNEL_USER = "channelupdatesbot";
-
-    public static final String RAE_TOKEN = "<token>";
-    public static final String RAE_USER = "raebot";
-
-    public static final String WEBHOOK_TOKEN = "<token>";
-    public static final String WEBHOOK_USER = "webhooksamplebot";
-
-    public static final String COMMANDS_TOKEN = "<token>";
-    public static final String COMMANDS_USER = "MyCommandsBot";
-    
-    public static final String ELEKTROLLART_TOKEN = "<token>";
-    public static final String ELEKTROLLART_USER = "ElektrollArtFanBot";
+    public static final String WEBHOOK_USER = WEATHER_USER;
+    public static final String WEBHOOK_TOKEN = WEATHER_TOKEN;
 }
--- src/main/java/org/telegram/Main.java
+++ src/main/java/org/telegram/Main.java
@@ -21,77 +21,88 @@ public class Main {
     private static final String LOGTAG = "MAIN";
 
     public static void main(String[] args) {
-        BotLogger.setLevel(Level.ALL);
-        BotLogger.registerLogger(new ConsoleHandler());
+        // Initialize Api Context
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
         try {
-            BotLogger.registerLogger(new BotsFileHandler());
-        } catch (IOException e) {
-            BotLogger.severe(LOGTAG, e);
+            botsApi.registerBot(new WeatherHandlers());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
-
-        try {
-            ApiContextInitializer.init();
-            TelegramBotsApi telegramBotsApi = createTelegramBotsApi();
-            try {
-                // Register long polling bots. They work regardless type of TelegramBotsApi we are creating
-                telegramBotsApi.registerBot(new WeatherHandlers());
-            } catch (TelegramApiException e) {
-                BotLogger.error(LOGTAG, e);
-            }
-        } catch (Exception e) {
-            BotLogger.error(LOGTAG, e);
-        }
+//        BotLogger.setLevel(Level.ALL);
+//        BotLogger.registerLogger(new ConsoleHandler());
+//        try {
+//            BotLogger.registerLogger(new BotsFileHandler());
+//        } catch (IOException e) {
+//            BotLogger.severe(LOGTAG, e);
+//        }
+//
+//        try {
+//            ApiContextInitializer.init();
+//            TelegramBotsApi telegramBotsApi = createTelegramBotsApi();
+//            try {
+//                // Register long polling bots. They work regardless type of TelegramBotsApi we are creating
+//                telegramBotsApi.registerBot(new WeatherHandlers());
+//            } catch (TelegramApiException e) {
+//                BotLogger.error(LOGTAG, e);
+//            }
+//        } catch (Exception e) {
+//            BotLogger.error(LOGTAG, e);
+//        }
     }
 
-    private static TelegramBotsApi createTelegramBotsApi() throws TelegramApiException {
-        TelegramBotsApi telegramBotsApi;
-        if (!BuildVars.useWebHook) {
-            // Default (long polling only)
-            telegramBotsApi = createLongPollingTelegramBotsApi();
-        } else if (!BuildVars.pathToCertificatePublicKey.isEmpty()) {
-            // Filled a path to a pem file ? looks like you're going for the self signed option then, invoke with store and pem file to supply.
-            telegramBotsApi = createSelfSignedTelegramBotsApi();
-            telegramBotsApi.registerBot(new WebHookExampleHandlers());
-        } else {
-            // Non self signed, make sure you've added private/public and if needed intermediate to your cert-store.
-            telegramBotsApi = createNoSelfSignedTelegramBotsApi();
-            telegramBotsApi.registerBot(new WebHookExampleHandlers());
-        }
-        return telegramBotsApi;
-    }
-
-    /**
-     * @brief Creates a Telegram Bots Api to use Long Polling (getUpdates) bots.
-     * @return TelegramBotsApi to register the bots.
-     */
-    private static TelegramBotsApi createLongPollingTelegramBotsApi() {
-        return new TelegramBotsApi();
-    }
-
-    /**
-     * @brief Creates a Telegram Bots Api to use Long Polling bots and webhooks bots with self-signed certificates.
-     * @return TelegramBotsApi to register the bots.
-     *
-     * @note https://core.telegram.org/bots/self-signed#java-keystore for generating a keypair in store and exporting the pem.
-    *  @note Don't forget to split the pem bundle (begin/end), use only the public key as input!
-     */
-    private static TelegramBotsApi createSelfSignedTelegramBotsApi() throws TelegramApiException {
-        return new TelegramBotsApi(BuildVars.pathToCertificateStore, BuildVars.certificateStorePassword, BuildVars.EXTERNALWEBHOOKURL, BuildVars.INTERNALWEBHOOKURL, BuildVars.pathToCertificatePublicKey);
-    }
-
-    /**
-     * @brief Creates a Telegram Bots Api to use Long Polling bots and webhooks bots with no-self-signed certificates.
-     * @return TelegramBotsApi to register the bots.
-     *
-     * @note Coming from a set of pem files here's one way to do it:
-     * @code{.sh}
-     * openssl pkcs12 -export -in public.pem -inkey private.pem > keypair.p12
-     * keytool -importkeystore -srckeystore keypair.p12 -destkeystore server.jks -srcstoretype pkcs12
-     * #have (an) intermediate(s) to supply? first:
-     * cat public.pem intermediate.pem > set.pem (use set.pem as -in)
-     * @endcode
-     */
-    private static TelegramBotsApi createNoSelfSignedTelegramBotsApi() throws TelegramApiException {
-        return new TelegramBotsApi(BuildVars.pathToCertificateStore, BuildVars.certificateStorePassword, BuildVars.EXTERNALWEBHOOKURL, BuildVars.INTERNALWEBHOOKURL);
-    }
+//    private static TelegramBotsApi createTelegramBotsApi() throws TelegramApiException {
+//        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+//        if (!BuildVars.useWebHook) {
+//            // Default (long polling only)
+//            telegramBotsApi = createLongPollingTelegramBotsApi();
+//        } else if (!BuildVars.pathToCertificatePublicKey.isEmpty()) {
+//            // Filled a path to a pem file ? looks like you're going for the self signed option then, invoke with store and pem file to supply.
+//            telegramBotsApi.registerBot(new WebHookExampleHandlers());
+//        } else {
+//            // Non self signed, make sure you've added private/public and if needed intermediate to your cert-store.
+//            telegramBotsApi = createNoSelfSignedTelegramBotsApi();
+//            telegramBotsApi.registerBot(new WebHookExampleHandlers());
+//        }
+//        return telegramBotsApi;
+//    }
+//
+//    /**
+//     * @brief Creates a Telegram Bots Api to use Long Polling (getUpdates) bots.
+//     * @return TelegramBotsApi to register the bots.
+//     */
+//    private static TelegramBotsApi createLongPollingTelegramBotsApi() {
+//        return new TelegramBotsApi();
+//    }
+//
+//    /**
+//     * @brief Creates a Telegram Bots Api to use Long Polling bots and webhooks bots with self-signed certificates.
+//     * @return TelegramBotsApi to register the bots.
+//     *
+//     * @note https://core.telegram.org/bots/self-signed#java-keystore for generating a keypair in store and exporting the pem.
+//    *  @note Don't forget to split the pem bundle (begin/end), use only the public key as input!
+//     */
+//    private static TelegramBotsApi createSelfSignedTelegramBotsApi() throws TelegramApiException {
+//        return new TelegramBotsApi(BuildVars.pathToCertificateStore, BuildVars.certificateStorePassword, BuildVars.EXTERNALWEBHOOKURL, BuildVars.INTERNALWEBHOOKURL, BuildVars.pathToCertificatePublicKey);
+//    }
+//
+//    /**
+//     * @brief Creates a Telegram Bots Api to use Long Polling bots and webhooks bots with no-self-signed certificates.
+//     * @return TelegramBotsApi to register the bots.
+//     *
+//     * @note Coming from a set of pem files here's one way to do it:
+//     * @code{.sh}
+//     * openssl pkcs12 -export -in public.pem -inkey private.pem > keypair.p12
+//     * keytool -importkeystore -srckeystore keypair.p12 -destkeystore server.jks -srcstoretype pkcs12
+//     * #have (an) intermediate(s) to supply? first:
+//     * cat public.pem intermediate.pem > set.pem (use set.pem as -in)
+//     * @endcode
+//     */
+//    private static TelegramBotsApi createNoSelfSignedTelegramBotsApi() throws TelegramApiException {
+//        return new TelegramBotsApi(BuildVars.pathToCertificateStore, BuildVars.certificateStorePassword, BuildVars.EXTERNALWEBHOOKURL, BuildVars.INTERNALWEBHOOKURL);
+//    }
 }
